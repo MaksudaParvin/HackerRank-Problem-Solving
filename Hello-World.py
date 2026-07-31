498e9d20d6d996a010d4bc2ad25135cdcff3d753
Maksuda Parvin <22234103375@cse.bubt.edu.bd>
2026-07-31 17:02:52 +0600

    Problem "Hello, World!"

---
+++ Hello-World.py
@@ -0,0 +1,16 @@
+"""
+Problem : Say "Hello, World!" With Python
+Platform: HackerRank
+Domain   : Python
+Difficulty: Easy
+Language : Python 3
+
+Problem Link:
+https://www.hackerrank.com/challenges/py-hello-world/problem
+
+Author   : Maksuda Parvin
+Date     : 2026-07-31
+"""
+
+my_string="Hello, World!"
+print(my_string)
