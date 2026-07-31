a9bb7d3fa447771f7673a6d963c12fee0dcd5e1c
Maksuda Parvin <22234103375@cse.bubt.edu.bd>
2026-07-31 18:21:52 +0600

    Problem Print-the-Elements-of-a-Linked-List

---
+++ Data Structure/Print-the-Elements-of-a-Linked-List.py
@@ -0,0 +1,18 @@
+"""
+------------------------------------------------------------
+Problem    : Print the Elements of a Linked List
+Platform   : HackerRank
+Domain     : Data Structures
+Category   : Linked Lists
+Difficulty : Easy
+Language   : Python 3
+
+Description:
+Given the head of a singly linked list, traverse the
+list from head to tail and print the data stored in
+each node, one value per line.
+
+Problem Link:
+https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem
+------------------------------------------------------------
+"""
