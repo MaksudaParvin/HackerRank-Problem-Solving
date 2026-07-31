5f38f1ff1150c20eddb4edd9dc97cce1ff7ea721
Maksuda Parvin <22234103375@cse.bubt.edu.bd>
2026-07-31 17:44:39 +0600

    Problem Python-Division

---
+++ Python/Python-Division.py
@@ -0,0 +1,22 @@
+"""
+------------------------------------------------------------
+Problem    : Python: Division
+Platform   : HackerRank
+Domain     : Python
+Category   : Introduction
+Difficulty : Easy
+Language   : Python 3
+
+Description:
+Read two integers and print the results of integer
+division (//) and float division (/) on separate lines.
+
+Problem Link:
+https://www.hackerrank.com/challenges/python-division/problem
+------------------------------------------------------------
+"""
+
+a = int(input())
+b = int(input())
+print(int(a/b))
+print(a/b)
