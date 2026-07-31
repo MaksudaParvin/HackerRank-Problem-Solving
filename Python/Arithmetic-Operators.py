8f1dc6d925c5ef9cc5c87295f7149b6e5a890e5a
Maksuda Parvin <22234103375@cse.bubt.edu.bd>
2026-07-31 17:40:38 +0600

    Problem Arithmetic-Operators

---
+++ Python/Arithmetic-Operators.py
@@ -0,0 +1,23 @@
+"""
+------------------------------------------------------------
+Problem    : Arithmetic Operators
+Platform   : HackerRank
+Domain     : Python
+Category   : Introduction
+Difficulty : Easy
+Language   : Python 3
+
+Description:
+Read two integers and print their sum, difference,
+and product on separate lines.
+
+Problem Link:
+https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+------------------------------------------------------------
+"""
+
+a = int(input())
+b = int(input())
+print(a+b)
+print(a-b)
+print(a*b)
